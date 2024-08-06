fd4cc89ef0d42decf86af0bb0cef3d6832073fce
3mar-04 <omar_sabri2004@icloud.com>
2024-08-06 23:14:58 +0300

    day-1

---
+++ portfolio/JS/main.js
@@ -0,0 +1,5 @@
+import '../Styles/style.css';
+import '../Styles/sections/header.css' ;
+import '../Styles/sections/main_me.css' ;
+import '../Styles/sections/lazy.css' ;
+import '../Styles/sections/list.css' ;
